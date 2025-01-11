73f4f3dfc9bf1de79ea6f6faa4fd09e8ef082114
giulioandrea <99687595+giulioandrea@users.noreply.github.com>
2025-01-11 17:48:08 +0100

    Update setup.py
    Now compiles cuda modules

--- setup.py
+++ setup.py
@@ -1,29 +1,91 @@
-# Copyright (c) 2021-2022 Patricio Cubillos
-# Pyrat Bay is open-source software under the GNU GPL-2.0 license (see LICENSE)
+# Copyright (c) 2021-2024 Patricio Cubillos
+# Pyrat Bay is open-source software under the GPL-2.0 license (see LICENSE)
 
 import os
 import re
-import sys
+import subprocess
 import setuptools
 from setuptools import setup, Extension
-
+from setuptools.command.build_ext import build_ext
+import numpy as np
 from numpy import get_include
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'pyratbay'))
-from version import __version__
-
-
+# Source directories
 srcdir = 'src_c/'  # C-code source folder
-incdir = 'src_c/include/'  # Include filder with header files
+incdir = 'src_c/include/'  # Include folder with header files
 
+# Detect CUDA installation
+def find_cuda():
+    if os.name == 'nt':  # Windows
+        cuda_path = os.environ.get('CUDA_PATH')
+        if cuda_path is None:
+            # Try standard installation path
+            if os.path.exists('C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA'):
+                for ver in ['v12.0', 'v11.0', 'v10.0']:
+                    if os.path.exists(f'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\{ver}'):
+                        cuda_path = f'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\{ver}'
+                        break
+    else:  # Linux/Mac
+        cuda_path = '/usr/local/cuda'
+        if not os.path.exists(cuda_path):
+            cuda_path = '/usr/cuda'
+            if not os.path.exists(cuda_path):
+                try:
+                    nvcc_path = subprocess.check_output(['which', 'nvcc']).decode().strip()
+                    cuda_path = os.path.dirname(os.path.dirname(nvcc_path))
+                except:
+                    return None
+    
+    if cuda_path is None:
+        return None
+    
+    return {
+        'home': cuda_path,
+        'nvcc': os.path.join(cuda_path, 'bin', 'nvcc'),
+        'include': os.path.join(cuda_path, 'include'),
+        'lib64': os.path.join(cuda_path, 'lib64'),
+    }
+
+# Custom build command that handles both C and CUDA extensions
+class CustomBuildExt(build_ext):
+    def build_extensions(self):
+        # First try to find CUDA
+        cuda = find_cuda()
+        
+        # Build regular C extensions
+        build_ext.build_extensions(self)
+        
+        # If CUDA is available, build CUDA extension
+        if cuda is not None:
+            try:
+                # Create lib directory if it doesn't exist
+                lib_dir = os.path.join(self.build_lib, 'pyratbay', 'lib')
+                os.makedirs(lib_dir, exist_ok=True)
+                
+                # Compile CUDA code
+                self.compiler.spawn([
+                    cuda['nvcc'],
+                    '-O3',
+                    '--shared',
+                    '--compiler-options', "'-fPIC'",
+                    '-o', os.path.join(lib_dir, 'spectrum_cuda.so'),
+                    os.path.join(srcdir, 'spectrum_cuda.cu'),
+                ])
+            except Exception as e:
+                print(f"CUDA compilation failed: {e}")
+                print("Continuing without CUDA support")
+
+# Get list of C source files
 cfiles = os.listdir(srcdir)
 cfiles = list(filter(lambda x: re.search('.+[.]c$', x), cfiles))
 cfiles = list(filter(lambda x: not re.search('[.#].+[.]c$', x), cfiles))
 
+# Setup include directories and compilation flags
 inc = [get_include(), incdir]
 eca = ['-ffast-math']
 ela = []
 
+# Create Extension instances for C files
 extensions = [
     Extension(
         'pyratbay.lib.' + cfile.rstrip('.c'),
@@ -35,104 +97,12 @@ extensions = [
     for cfile in cfiles
 ]
 
-long_description = """
-.. image:: https://raw.githubusercontent.com/pcubillos/pyratbay/master/docs/figures/pyrat_logo.png
-   :width: 35%
-
-|Build Status|  |docs|  |PyPI|  |conda|  |License|
-
-``Pyrat Bay``: Python Radiative Transfer in a Bayesian framework
-
-
-Install as:
-
-.. code-block:: shell
-
-    pip install pyratbay
-
-Or:
-
-.. code-block:: shell
-
-    conda install -c conda-forge pyratbay
-
-Docs at: https://github.com/pcubillos/pyratbay
-
-Cite as:
-
-.. code-block:: bibtex
-
-    @ARTICLE{CubillosBlecic2021mnrasPyratBay,
-           author = {{Cubillos}, Patricio E. and {Blecic}, Jasmina},
-            title = "{The PYRAT BAY framework for exoplanet atmospheric modelling: a population study of Hubble/WFC3 transmission spectra}",
-          journal = {\mnras},
-         keywords = {radiative transfer, methods: statistical, planets and satellites: atmosphere, Astrophysics - Earth and Planetary Astrophysics, Astrophysics - Instrumentation and Methods for Astrophysics},
-             year = 2021,
-            month = aug,
-           volume = {505},
-           number = {2},
-            pages = {2675-2702},
-              doi = {10.1093/mnras/stab1405},
-    archivePrefix = {arXiv},
-           eprint = {2105.05598},
-     primaryClass = {astro-ph.EP},
-           adsurl = {https://ui.adsabs.harvard.edu/abs/2021MNRAS.505.2675C},
-          adsnote = {Provided by the SAO/NASA Astrophysics Data System}
-    }
-
-.. |Build Status| image:: https://travis-ci.com/pcubillos/pyratbay.svg?branch=master
-   :target: https://travis-ci.com/pcubillos/pyratbay
-
-.. |docs| image:: https://readthedocs.org/projects/pyratbay/badge/?version=latest
-    :target: https://pyratbay.readthedocs.io/en/latest/?badge=latest
-    :alt: Documentation Status
-
-.. |PyPI| image:: https://img.shields.io/pypi/v/pyratbay.svg
-    :target:      https://pypi.org/project/pyratbay/
-    :alt: Latest Version
-
-.. |conda| image:: https://img.shields.io/conda/vn/conda-forge/pyratbay.svg
-    :target: https://anaconda.org/conda-forge/pyratbay
-
-.. |License| image:: https://img.shields.io/github/license/pcubillos/pyratbay.svg?color=blue
-    :target: https://pyratbay.readthedocs.io/en/latest/license.html
-"""
-
-
-package_data = {'pyratbay': [
-    'data/AsplundEtal2009.txt',
-    'data/atoms.dat',
-    'data/molecules.dat',
-    'data/isotopes.dat',
-    'data/tips_2021.pkl',
-    'data/filters/*.dat',
-    'data/CIA/*.dat',
-    ]}
-
-
+# Main setup configuration
 setup(
-    name = 'pyratbay',
-    version = __version__,
-    author = 'Patricio Cubillos',
-    author_email = 'patricio.cubillos@oeaw.ac.at',
-    url = 'https://github.com/pcubillos/pyratbay',
-    packages = setuptools.find_packages(),
-    package_data = package_data,
-    install_requires = [
-        'numpy>=1.15.1',
-        'scipy>=1.4.1',
-        'matplotlib>=3.3.4',
-        'mc3>=3.0.7',
-        'chemcat>=0.3.1',
-        ],
-    tests_require = [
-        'pytest>=3.9',
-        ],
-    license = 'GPLv2',
-    description = 'Python Radiative Transfer in a Bayesian Framework.',
-    long_description = long_description,
-    long_description_content_type = 'text/x-rst',
-    include_dirs = inc,
-    entry_points = {'console_scripts': ['pbay = pyratbay.__main__:main']},
-    ext_modules = extensions,
+    name='pyratbay',
+    ext_modules=extensions,
+    include_dirs=inc,
+    cmdclass={
+        'build_ext': CustomBuildExt,
+    },
 )
